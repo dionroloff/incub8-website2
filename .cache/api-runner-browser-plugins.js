ea591050b5ed59f94e36ba7b7616beddf4918e58
James Tucker <jamesctucker@protonmail.com>
2019-04-05 15:26:26 -0500

    tweaked website intro

---
+++ .cache/api-runner-browser-plugins.js
@@ -0,0 +1,7 @@
+module.exports = [{
+      plugin: require('/Users/jamestucker/incub8-website2/node_modules/gatsby-plugin-offline/gatsby-browser.js'),
+      options: {"plugins":[]},
+    },{
+      plugin: require('/Users/jamestucker/incub8-website2/gatsby-browser.js'),
+      options: {"plugins":[]},
+    }]
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,24 +1,25 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+// import logo from '../images/logo.png';
 
 const Header = (props) => (
-    <header id="header" style={props.timeout ? {display: 'none'} : {}}>
+    <header id="header" style={props.timeout ? { display: 'none' } : {}}>
         <div className="logo">
             <span className="icon fa-diamond"></span>
+            {/* <img src={logo} alt="incub8-logo" /> */}
         </div>
         <div className="content">
             <div className="inner">
-                <h1>Dimension</h1>
-                <p>A fully responsive site template designed by <a href="https://html5up.net">HTML5 UP</a> and released<br />
-                for free under the <a href="https://html5up.net/license">Creative Commons</a> license.</p>
+                <h1>Coding for good.</h1>
+                <p>We build beautiful websites and apps for fledgling organizations who care about social good.</p>
             </div>
         </div>
         <nav>
             <ul>
-                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('intro')}}>Intro</a></li>
-                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('work')}}>Work</a></li>
-                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('about')}}>About</a></li>
-                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('contact')}}>Contact</a></li>
+                <li><a href="javascript:;" onClick={() => { props.onOpenArticle('intro') }}>About</a></li>
+                <li><a href="javascript:;" onClick={() => { props.onOpenArticle('work') }}>Work</a></li>
+                <li><a href="javascript:;" onClick={() => { props.onOpenArticle('about') }}>Team</a></li>
+                <li><a href="javascript:;" onClick={() => { props.onOpenArticle('contact') }}>Contact</a></li>
             </ul>
         </nav>
     </header>
